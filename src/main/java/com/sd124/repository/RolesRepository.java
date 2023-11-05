0600812f7772b9810d68924c175ba7b9e18b349d
TruongSon-PT <86681834+TruongSon-PT@users.noreply.github.com>
2023-11-05 14:15:04 +0700

    Create RolesRepository.java

---
+++ src/main/java/com/sd124/repository/RolesRepository.java
@@ -0,0 +1,9 @@
+package com.sd124.repository;
+
+import com.sd124.model.Roles;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RolesRepository extends JpaRepository<Roles, Integer> {
+}
