86d9ff855abe55df1848ed6a4375b0c66568c593
TruongSon-PT <86681834+TruongSon-PT@users.noreply.github.com>
2023-11-10 11:40:22 +0700

    Create ProductAdminController.java

---
+++ src/main/java/com/sd124/controller/customer/ProductAdminController.java
@@ -0,0 +1,36 @@
+package com.sd124.controller.customer;
+
+import com.sd124.model.Products;
+import com.sd124.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin/product")
+public class ProductAdminController {
+    @Autowired
+    private ProductRepository productRepo;
+
+    @GetMapping("")
+    public String listProduct(Model model){
+        List<Products> lstPro = productRepo.findAll();
+        model.addAttribute("productList", lstPro);
+        return "admin/product/index";
+    }
+
+    @GetMapping("/add")
+    public String addProduct(){
+        return "admin/product/form";
+    }
+
+    @GetMapping("/update/{id}")
+    public  String updateProduct(@PathVariable Integer id) {
+        return "admin/product/form";
+    }
+}
