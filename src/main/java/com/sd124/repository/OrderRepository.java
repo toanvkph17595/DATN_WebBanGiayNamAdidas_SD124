4b4692c155c484c1786f8a189a6ba0b4eda39c8f
TruongSon-PT <86681834+TruongSon-PT@users.noreply.github.com>
2023-11-16 00:55:50 +0700

    Update OrderRepository.java

--- src/main/java/com/sd124/repository/OrderRepository.java
+++ src/main/java/com/sd124/repository/OrderRepository.java
@@ -8,10 +8,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+import java.util.Date;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Orders, Integer> {
     @Query("SELECT o FROM Orders o WHERE o.status = :status ORDER BY o.createdDate DESC ")
     Page<Orders> findOrder(@Param("status") Integer status, Pageable pageable);
+
+    @Query("SELECT o FROM Orders o WHERE o.id = :id OR o.userName.fullName like :search OR o.recipientPhone like :search AND (o.createdDate between :startDate and :endDate) ORDER BY o.createdDate DESC ")
+    Page<Orders> findOrdersBySearch(@Param("id") Integer id, @Param("search") String search, @Param("startDate") LocalDate startDate, @Param("endDate") LocalDate endDate, Pageable pageable);
 
     @Query("SELECT o FROM Orders o ORDER BY o.createdDate DESC ")
     Page<Orders> findAllOrder(Pageable pageable);
